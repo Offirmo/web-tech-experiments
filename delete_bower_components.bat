383fa535a9b842c8caad1ef381d945fd86da107e
Offirmo <offirmo.net@gmail.com>
2013-10-23 01:09:01 +0200

    new windows cleaning scripts

---
+++ delete_bower_components.bat
@@ -0,0 +1,6 @@
+rmdir app\bower_components /s /q
+rmdir "app\other_components\base-objects.js\test_runner\bower_components" /s /q
+rmdir "app\other_components\extended-exceptions.js\test_runner\bower_components" /s /q
+rmdir "app\other_components\network-constants.js\test_runner\bower_components" /s /q
+rmdir "app\other_components\restlink.js\test_runner\bower_components" /s /q
+pause
