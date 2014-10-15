895b159419dd64854d8e87afbf4886944c985b3f
Offirmo <offirmo.net@gmail.com>
2014-10-15 03:26:07 +0200

    WW improve

--- client/webworkers_003.js
+++ client/webworkers_003.js
@@ -2,27 +2,38 @@
 self.requirejs_baseurl = 'http://172.30.2.160:3000/';
 //self.requirejs_baseurl = 'http://192.168.56.100:3000/';
 
-console.log('hello from webworker ! starting from : ' + location);
-//console.error('with : ' + JSON.stringify(navigator));
+// https://developer.mozilla.org/en-US/docs/Web/API/WorkerLocation
+console.log('hello from webworker ! starting from : ' + self.location);
 
-/*
-self.onmessage = function(e) {
-	console.log('worker : seen message from parent : ' + JSON.stringify(e));
-};*/
+// https://developer.mozilla.org/en-US/docs/Web/API/WorkerNavigator
+console.log('hello from webworker ! with : ' + JSON.stringify(self.navigator));
 
 //console.log('DaF with console ?', 'show me !');
 
-//console.log(this);
-//debugger;
+// this one will be replaced later
+self.onmessage = function(e) {
+	console.log('worker : seen message from parent while I was initializing: ', e.data); //, JSON.stringify(e));
+};
 
+self.onerror = function(e) {
+	console.log('worker : seen "error" message', arguments);
+};
+
+self.ononline = function() {
+	console.log('worker : seen "online" message');
+};
+
+self.onoffline = function() {
+	console.log('worker : seen "offline" message');
+};
+
+self.onclose = function() {
+	console.log('worker : seen "close" message');
+};
 
 
 importScripts('bower_components/requirejs/require.js');
 importScripts('misc/components.js');
-
-//importScripts('bower_components/javascript-state-machine/state-machine.js');
-//importScripts('bower_components/accounting/accounting.js');
-
 
 requirejs(
 [
@@ -44,6 +55,8 @@ function(StateMachine) {
 	fsm.warn();
 
 	console.log('Hello from required worker !');
+
+	//throw new Error('foo');
 
 	self.onmessage = function(e) {
 		console.log('worker : seen message from parent : ', e.data); //, JSON.stringify(e));
