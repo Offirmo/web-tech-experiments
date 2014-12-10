9cbbd534401c96f026058490a694ecfe25d7ae58
Offirmo <offirmo.net@gmail.com>
2014-12-10 10:55:06 +0100

    experiment on RoR architecture

--- server/express4_002_basic/client/ror/client.js
+++ server/express4_002_basic/client/ror/client.js
@@ -1,9 +1,38 @@
 define(
 [
-	'lodash'
+	'lodash',
+	'logator',
 ],
-function(_) {
+function(_, Logator) {
 	'use strict';
 
+	function RorClient(server, options) {
+		// scan options
+		options = options || {};
 
+		var logger = options.logger || Logator.make_new();
+
+		logger.log('Instantiating a new RoR client...');
+
+		var state = {
+		};
+
+		var server_interface = {
+			send_response: function() {
+
+			}
+		};
+		server.add_client(server_interface);
+
+		var ui_interface = {
+			click: function(action_id) {
+				// TODO
+			}
+		};
+
+	}
+
+	return {
+		make_new: function(server, options) { return new RorClient(server, options); }
+	};
 });
