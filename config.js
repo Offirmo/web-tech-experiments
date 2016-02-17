3434949136d38bd36bf02d341d02c32df33b063d
Offirmo <offirmo.net@gmail.com>
2016-02-17 23:25:37 +0000

    +

--- config.js
+++ config.js
@@ -8,6 +8,7 @@ System.config({
       "optimisation.modules.system"
     ]
   },
+
   paths: {
     "github:*": "jspm_packages/github/*",
     "npm:*": "jspm_packages/npm/*",
@@ -15,7 +16,11 @@ System.config({
   },
 
   map: {
+    "angular": "github:angular/bower-angular@1.5.0",
+    "angular-animate": "github:angular/bower-angular-animate@1.5.0",
+    "angular-aria": "github:angular/bower-angular-aria@1.5.0",
     "angular-material": "github:angular/bower-material@1.0.5",
+    "angular-messages": "github:angular/bower-angular-messages@1.5.0",
     "babel": "npm:babel-core@5.8.35",
     "babel-runtime": "npm:babel-runtime@5.8.35",
     "bootstrap": "github:twbs/bootstrap@3.3.6",
@@ -40,6 +45,9 @@ System.config({
       "angular": "github:angular/bower-angular@1.5.0"
     },
     "github:angular/bower-angular-aria@1.5.0": {
+      "angular": "github:angular/bower-angular@1.5.0"
+    },
+    "github:angular/bower-angular-messages@1.5.0": {
       "angular": "github:angular/bower-angular@1.5.0"
     },
     "github:angular/bower-material@1.0.5": {
@@ -199,5 +207,13 @@ System.config({
       "inherits": "npm:inherits@2.0.1",
       "process": "github:jspm/nodelibs-process@0.1.2"
     }
-  }
+  },
+
+	"meta": {
+		"github:angular/bower-angular-messages@1.5.0": {
+			"deps": [
+				"angular"
+			]
+		}
+	}
 });
