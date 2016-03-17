107be2bc5c6477f4023381f87cdfe24fd74f4ff9
Offirmo <offirmo.net@gmail.com>
2016-03-17 22:14:29 +0100

    +

--- nodejs/fw_es6_001_node5_vanilla/index.js
+++ nodejs/fw_es6_001_node5_vanilla/index.js
@@ -1,107 +1,37 @@
 #!/bin/sh
-':' //# http://sambal.org/?p=1014 ; exec `dirname $0`/../../../node_modules/.bin/babel-node "$0" "$@"
+':' //# http://sambal.org/?p=1014 ; exec /usr/bin/env node --harmony_modules --harmony_regexps --harmony_proxies --harmony_sloppy_function --harmony_sloppy_let --harmony_unicode_regexps --harmony_reflect --harmony_destructuring --harmony_default_parameters --harmony_sharedarraybuffer --harmony_atomics --harmony_simd "$0" "$@"
 'use strict';
+
+// https://nodejs.org/en/docs/es6/
+// node --v8-options | grep "in progress"
 
 const fs = require('fs');
 
-console.error(`------- code outside of the loop -------`);
 
 // to work, the following code MUST be called before anything else
 let eventLoopCount = 0;
 function eventLoopDetector() {
-  if (eventLoopCount === 0) {
-    // we are not yet in the loop
-  }
-  else {
-    // we are in the loop
-    if (eventLoopCount === 1)
-      console.error(`
+	if (eventLoopCount === 0) {
+		// we are not yet in the loop
+	}
+	else {
+		// we are in the loop
+		if (eventLoopCount === 1)
+			console.error(`
 ------- the event loop starts ! -------`);
-    else
-      console.error(`
+		else
+			console.error(`
 ------- event loop #${eventLoopCount} -------`);
-    console.error('--- 1) timeouts / intervals :');
-  }
+		console.error('--- 1) timeouts / intervals :');
+	}
 
-  if (eventLoopCount !== 1) {
-    fs.stat('foo', () => console.error('--- 2) I/O callbacks :'));
-    setImmediate(() => console.error('--- 3) immediates :'));
-  }
+	if (eventLoopCount !== 1) {
+		fs.stat('foo', () => console.error('--- 2) I/O callbacks :'));
+		setImmediate(() => console.error('--- 3) immediates :'));
+	}
 
-  eventLoopCount++;
-  if (eventLoopCount < 12)
-    setTimeout(eventLoopDetector);
+	eventLoopCount++;
+	if (eventLoopCount < 12)
+		setTimeout(eventLoopDetector);
 }
 eventLoopDetector();
-
-setUpDelayed('->'); // direct
-
-
-
-
-let setNextTickCount = 0;
-function setNextTickDetector() {
-  if (setNextTickCount > 0) {
-    console.error(`- setNextTick #${setNextTickCount}`);
-  }
-  setNextTickCount++;
-  if (setNextTickCount < 10)
-    process.nextTick(setNextTickDetector);
-}
-process.nextTick(setNextTickDetector);
-
-
-let setImmediateCount = 0;
-function setImmediateDetector() {
-  if (setImmediateCount > 0) {
-    console.error(`- setImmediate #${setImmediateCount}`);
-  }
-  setImmediateCount++;
-  if (setImmediateCount < 10) {
-    setImmediate(setImmediateDetector);
-    //process.nextTick(() => setImmediate(() => console.error('nextTicked setImmediate')));
-  }
-}
-setImmediate(setImmediateDetector);
-
-
-
-let setTimeoutCount = 0;
-function setTimeoutDetector() {
-  setTimeoutCount++;
-  console.error(`- setTimeout #${setTimeoutCount}`);
-  if (setTimeoutCount < 10)
-    setTimeout(setTimeoutDetector);
-}
-setTimeout(setTimeoutDetector);
-
-let intervalObject;
-let setIntervalCount = 0;
-function setIntervalDetector() {
-  setIntervalCount++;
-  console.error(`- setInterval #${setIntervalCount}`);
-  if (setIntervalCount >= 10)
-    clearInterval(intervalObject);
-}
-intervalObject = setInterval(setIntervalDetector);
-
-
-function setUpDelayed(id) {
-  console.error(id, 'scheduling setImmediate...');
-  setImmediate(() => console.error(id, 'setImmediate !'));
-
-  console.error(id, 'scheduling setTimeout...');
-  setTimeout(() => console.error(id, 'setTimeout !'));
-
-  console.error(id, 'scheduling process.nextTick...');
-  process.nextTick(() => console.error(id, 'process.nextTick !'));
-}
-
-
-/*
-setImmediate(() => setUpDelayed('from setImmediate ->'));
-
-setNextTick(() => setUpDelayed('from setNextTick ->'));
-
-process.nextTick(() => setUpDelayed('from process.nextTick ->'));
-*/
