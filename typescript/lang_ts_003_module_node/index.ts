40b94a15d7920a2179d582b013fecc112f981621
Offirmo <offirmo.net@gmail.com>
2016-07-06 14:44:16 +0200

    +

--- typescript/lang_ts_003_module_node/index.ts
+++ typescript/lang_ts_003_module_node/index.ts
@@ -1,6 +1,8 @@
 #!/bin/sh
 ':' //# http://sambal.org/?p=1014 ; exec `dirname $0`/../../node_modules/.bin/ts-node "$0" "$@"
 
-import * as _ from 'lodash'
+//import * as _ from 'lodash'
+//const _ = require('lodash')
+import _ = require('lodash')
 
 console.log(_.toUpper('Hello, world !'))
---
+++ typescript/notes.md
@@ -0,0 +1,12 @@
+
+
+		"jsx": "react",
+		"noFallthroughCasesInSwitch": true,
+		"noImplicitAny": true,
+		"noImplicitReturns": true,
+		"suppressImplicitAnyIndexErrors": true,
+		
+/*
+"moduleResolution": "node",
+"module": "commonjs",
+*/
