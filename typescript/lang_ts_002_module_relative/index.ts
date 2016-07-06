7fc3245c5cddb1a936a974fbd3bfea501a100572
Offirmo <offirmo.net@gmail.com>
2016-07-06 13:50:37 +0200

    +

---
+++ typescript/lang_ts_002_module_relative/index.ts
@@ -0,0 +1,6 @@
+#!/bin/sh
+':' //# http://sambal.org/?p=1014 ; exec `dirname $0`/../../node_modules/.bin/ts-node "$0" "$@"
+
+import hello from './lib'
+
+hello('typescript world')
---
+++ typescript/lang_ts_002_module_relative/lib.ts
@@ -0,0 +1,4 @@
+
+export default function hello(locutor: string): void {
+	console.log(`Hello, nice ${locutor} !`)
+}
