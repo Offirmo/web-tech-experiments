da3d4b25a0c975498f81870c4db0d2c7acf3c091
Offirmo <offirmo.net@gmail.com>
2017-01-17 14:57:46 +0700

    +

---
+++ nodejs/express4_012_http_basic_auth/index.js
@@ -0,0 +1,57 @@
+#!/usr/bin/env node
+'use strict';
+
+// a page protected by http basic auth
+// https://en.wikipedia.org/wiki/Basic_access_authentication
+
+console.log('Hello world !')
+
+var _ = require('lodash')
+
+// http://stackoverflow.com/questions/3653065/get-local-ip-address-in-node-js
+// http://nodejs.org/api/os.html#os_os_networkinterfaces
+var local_ips = _.chain(require('os').networkInterfaces())
+	.values()
+	.flatten()
+	.map('address')
+	.value()
+
+
+// http://expressjs.com/4x/api.html
+
+var listening_port = 3000
+var express = require('express')
+var app = express()
+
+
+function requireBasicHttpAuth(realm, res) {
+	res.writeHead(401, {
+		'WWW-Authenticate': 'Basic realm="' + realm + '"',
+		'Cache-Control': 'no-cache, no-store, must-revalidate',
+		'Pragma': 'no-cache',
+		'Expires': 0
+	})
+	res.end()
+}
+app.get('/', function(req, res) {
+	const allowed = false // so far
+
+	if (!req.headers.authorization) return requireBasicHttpAuth('offirmo-wte', res)
+
+	console.log(req.headers.authorization, req.headers.authorization.split('Basic '))
+
+	var currentAuthB64 = req.headers.authorization.split('Basic ')[1]
+	var currentAuth = new Buffer(currentAuthB64, 'base64').toString('utf8')
+	console.log(currentAuthB64, currentAuth)
+
+	if (currentAuth !== 'foo:bar') return requireBasicHttpAuth('offirmo-wte', res)
+
+	res.send('Successfully authenticated !')
+});
+
+_.forEach(local_ips, function(ip) {
+	console.log('Listening on http://' + ip + ':' + listening_port);
+});
+console.log('(Ctrl+C to stop)');
+
+app.listen(listening_port);
