b7b1ba9e389b680e866ae5fdcdbb8ac48204dbc4
Offirmo <offirmo.net@gmail.com>
2017-07-31 22:55:56 +1000

    TLDW :scream_cat:

--- typescript/mod_eventemitter3_001_base/index.ts
+++ typescript/mod_eventemitter3_001_base/index.ts
@@ -2,5 +2,12 @@
 ':' //# http://sambal.org/?p=1014 ; exec `dirname $0`/../../node_modules/.bin/ts-node "$0" "$@"
 console.log('Hello, world !')
 
-import EE = require('eventemitter3')
+import { ListenerFn, EventEmitter } from 'eventemitter3'
 
+
+const ee = new EventEmitter()
+
+ee.on('event', () => {
+	console.log('an event occurred!')
+})
+ee.emit('event')
