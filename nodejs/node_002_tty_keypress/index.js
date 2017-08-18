aa82c6fe1c866910ac2a7c9fbf7357b6b096cef4
Offirmo <offirmo.net@gmail.com>
2017-08-19 09:20:56 +1000

    renamings

---
+++ nodejs/node_002_tty_keypress/index.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+'use strict';
+
+console.log('Hello world ! Press any key combination...');
+
+const readline = require('readline')
+
+if (!process.stdin.setRawMode)
+	throw new Error('current term is not a tty !')
+
+readline.emitKeypressEvents(process.stdin)
+process.stdin.setRawMode(true)
+
+process.stdin.on('keypress', (str, key) => {
+	console.info('---- key pressed ----')
+	console.log(str)
+	console.log(key)
+})
